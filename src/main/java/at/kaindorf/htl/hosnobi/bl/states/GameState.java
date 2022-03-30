16f5fd99f680f1477a63ed11fbbc16a5c36deab7
brofld18 <brofld18@htl-kaindorf.at>
2022-03-30 15:35:53 +0200

    Started with GameStates

--- src/main/java/at/kaindorf/htl/hosnobi/bl/states/GameState.java
+++ src/main/java/at/kaindorf/htl/hosnobi/bl/states/GameState.java
@@ -1,21 +1,38 @@
 package at.kaindorf.htl.hosnobi.bl.states;
 
 import at.kaindorf.htl.hosnobi.bl.GameManager;
+import at.kaindorf.htl.hosnobi.bl.exceptions.IllegalStateMethodCallException;
 
 public abstract class GameState {
     public GameManager gameManager;
 
     private int playerCount;
     private int currentPlayer;
+    private boolean lastRound;
 
-    public abstract void AllUsersLoaded();
-    public abstract void NextPlayerTurn(int playerID);
-    public abstract void GameEnd();
-    public abstract void PlayerUsedCard(int playerID);
+    public  void PlayersLoaded() throws IllegalStateMethodCallException {
+        throw new IllegalStateMethodCallException();
+    }
+    public void ChoseCard(int oldCardId, int newCardId) throws IllegalStateMethodCallException {
+        throw new IllegalStateMethodCallException();
+    }
+    public void PlayerBlock() throws IllegalStateMethodCallException {
+        this.lastRound = true;
+    }
+    public void NextPlayer() throws IllegalStateMethodCallException {
+        throw new IllegalStateMethodCallException();
+    }
+    public abstract void EndGame();
 
     public GameState(GameManager gameManager, int playerCount) {
         this.playerCount = playerCount;
         this.gameManager = gameManager;
-        currentPlayer = 0;
+        this.currentPlayer = 0;
+        this.lastRound = false;
+    }
+    public GameState(GameState previousState) {
+        this.playerCount = previousState.playerCount;
+        this.gameManager = previousState.gameManager;
+        currentPlayer = previousState.currentPlayer;
     }
 }
