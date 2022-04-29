27bd9ad160a14daac7b979323c635f6802f03258
Moritz Zierler <ziemod18@htl-kaindorf.at>
2022-04-29 13:08:53 +0200

    Finished first version of lobby.html (previous "createGame.html").
    Added frontend.js and backend.js;

---
+++ src/main/webapp/frontend.js
@@ -0,0 +1,19 @@
+function fillPlayer(playerArr){
+
+}
+
+function setGameId(gameId){
+    document.getElementById("gameId").innerText(gameId);
+}
+
+function createLobby(){
+
+}
+
+function joinLobby(gameId){
+
+}
+
+function setSettings(){
+
+}
