ebc9411b3c21792a50d5570522ea56bee662314b
Moritz Zierler <ziemod18@htl-kaindorf.at>
2022-06-15 15:49:47 +0200

    game.html: buttons added frontend.js: cardClicked(cardNum) added

--- src/main/webapp/frontend.js
+++ src/main/webapp/frontend.js
@@ -1,3 +1,7 @@
+let card1toggle = -1;
+let card2toggle = -1;
+let card3toggle = -1;
+
 function fillPlayer(playerArr){
     document.getElementById("player1").innerText = playerArr[0];
     document.getElementById("player2").innerText = playerArr[1];
@@ -31,4 +35,46 @@ async function onLoad() {
 async function enterJoinLobbyScreen(username) {
     await createUser(username);
     window.open("joinGame.html", "_self");
+}
+
+function cardClicked(cardNum){
+
+    if(cardNum == 1){
+        card1toggle = card1toggle * (-1);
+        card2toggle = -1;
+        card3toggle = -1;
+    }
+
+    if(cardNum == 2){
+        card1toggle = -1;
+        card2toggle = card2toggle * (-1);
+        card3toggle = -1;
+    }
+
+    if(cardNum == 3){
+        card1toggle = -1;
+        card2toggle = -1;
+        card3toggle = card3toggle * (-1);
+    }
+
+    if(card1toggle == 1){
+        document.getElementById("card1").style.boxShadow = "0px 0px 15px 5px #ffffff";
+    }
+    else {
+        document.getElementById("card1").style.boxShadow = "0px 0px 15px 5px #606060";
+    }
+
+    if(card2toggle == 1){
+        document.getElementById("card2").style.boxShadow = "0px 0px 15px 5px #ffffff";
+    }
+    else {
+        document.getElementById("card2").style.boxShadow = "0px 0px 15px 5px #606060";
+    }
+
+    if(card3toggle == 1){
+        document.getElementById("card3").style.boxShadow = "0px 0px 15px 5px #ffffff";
+    }
+    else {
+        document.getElementById("card3").style.boxShadow = "0px 0px 15px 5px #606060";
+    }
 }
