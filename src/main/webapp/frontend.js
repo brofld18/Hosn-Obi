43f0da26f3c0be503dc7a7cc51e014d23f322725
Moritz Zierler <ziemod18@htl-kaindorf.at>
2022-06-21 16:21:45 +0200

    frontend.js: + lifeCounter(players) + endRound(points) + newRound(players)

--- src/main/webapp/frontend.js
+++ src/main/webapp/frontend.js
@@ -3,6 +3,7 @@ let card2toggle = -1;
 let card3toggle = -1;
 let cardsSwitched = 0;
 let blocked = 0;
+let roundEnd = 0;
 
 function fillPlayer(playerArr){
     document.getElementById("player1").innerText = playerArr[0];
@@ -200,7 +201,8 @@ function nextOne(){
 function block(){
     nextOne();
     blocked = 1;
-    document.getElementById('blocked1').innerText = ' - gesperrt';
+    document.getElementById('pointsAndState1').innerText = ' - gesperrt';
+    document.getElementById('pointsAndState1').style.color = "red";
     document.getElementById('player1').style.color = "red";
 }
 
@@ -246,21 +248,79 @@ function playerPlaying(id){
 
 function playerBlocked(id){
     if(id == 1){
-        document.getElementById('blocked1').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState1').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState1').style.color = "red";
         document.getElementById('player1').style.color = "red";
     }
     if(id == 2){
-        document.getElementById('blocked2').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState2').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState2').style.color = "red";
         document.getElementById('player2').style.color = "red";
     }
     if(id == 3){
-        document.getElementById('blocked3').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState3').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState3').style.color = "red";
         document.getElementById('player3').style.color = "red";
     }
     if(id == 4){
-        document.getElementById('blocked4').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState4').innerText = ' - gesperrt';
+        document.getElementById('pointsAndState4').style.color = "red";
         document.getElementById('player4').style.color = "red";
     }
+}
+
+function lifeCounter(players){
+    document.getElementById("player1lifes").innerText = players[0].lives;
+    document.getElementById("player2lifes").innerText = players[1].lives;
+    document.getElementById("player3lifes").innerText = players[2].lives;
+    document.getElementById("player4lifes").innerText = players[3].lives;
+}
+
+function endRound(points){
+    document.getElementById("pointsAndState1").innerText = ' - Punkte: ' + points[0];
+    document.getElementById("pointsAndState1").style.color = "white";
+    document.getElementById("pointsAndState2").innerText = ' - Punkte: ' + points[1];
+    document.getElementById("pointsAndState2").style.color = "white";
+    document.getElementById("pointsAndState3").innerText = ' - Punkte: ' + points[2];
+    document.getElementById("pointsAndState3").style.color = "white";
+    document.getElementById("pointsAndState4").innerText = ' - Punkte: ' + points[3];
+    document.getElementById("pointsAndState4").style.color = "white";
+
+    let highest = points[0];
+    let highestID = 1;
+    let lowest = points[0];
+    let lowestID = 1;
+
+    for(i = 1; i < points.length; i++) {
+        if(points[i] > highest) {
+            highest = points[i];
+            highestID = i+1;
+        }
+        if(points[i] < lowest){
+            lowest = points[i];
+            lowestID = i+1;
+        }
+    }
+
+    let highestPlayer = 'player' + highestID;
+    let highestPoints = 'pointsAndState' + highestID;
+    let lowestPlayer = 'player' + lowestID;
+    let lowestPoints = 'pointsAndState' + lowestID;
+
+    document.getElementById(highestPlayer).style.color = "green";
+    document.getElementById(highestPoints).style.color = "green";
+    document.getElementById(lowestPlayer).style.color = "red";
+    document.getElementById(lowestPoints).style.color = "red";
+}
+
+function newRound(players){
+    roundEnd = 0;
+    document.getElementById("pointsAndState1").innerText = '';
+    document.getElementById("pointsAndState2").innerText = '';
+    document.getElementById("pointsAndState3").innerText = '';
+    document.getElementById("pointsAndState4").innerText = '';
+
+    lifeCounter(players);
 }
 
 function Start() {
@@ -271,6 +331,9 @@ function Start() {
 }
 
 function SetGameManager(gameManager) {
+    newRound(gameManager.users);
+
+    /*
     document.getElementById("Player1Lives").innerText = "Leben: " + gameManager.users[0].lives;
     document.getElementById("Player2Lives").innerText = "Leben: " + gameManager.users[1].lives;
     document.getElementById("Player3Lives").innerText = "Leben: " + gameManager.users[2].lives;
@@ -280,11 +343,10 @@ function SetGameManager(gameManager) {
     document.getElementById("Player2Name").innerText = gameManager.users[1].username;
     document.getElementById("Player3Name").innerText = gameManager.users[2].username;
     document.getElementById("Player4Name").innerText = gameManager.users[3].username;
-
-    document.getElementById();
+    */
 }
 
 //ich gette Username - ich zeige Spieler hat gewonnen --> Moritz nach brunch
-//ich bekomme Lebencounter Array[4] --> Stelle 0 Spieler selbst --> Moritz nach brunch
+//ich bekomme Lebencounter Array[4] --> Stelle 0 Spieler selbst --> Flo Kontrolle
 
 //Wenn man selbst wieder dran ist die Variable cardsSwitched wieder auf 0 setzen. --> Flo bitte machen
